1ce3489333da234bdd51264af39785af280143d2
Lucas <lucaswinkler@gmail.com>
2020-03-07 00:32:02 -0500

    Auto updating chart

--- survey-polling.client/src/App.js
+++ survey-polling.client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Chat from './Components/Chat';
 import Poll from './Components/Poll';
 
 function App() {
   return (
     <div className='App'>
-      {/* <Chat /> */}
       <Poll />
     </div>
   );
--- survey-polling.client/src/Components/Poll.js
+++ survey-polling.client/src/Components/Poll.js
@@ -1,33 +1,47 @@
 ﻿import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const getState = () => ({
+  labels: ['Red', 'Green', 'Yellow'],
+  datasets: [
+    {
+      data: [
+        getRandomInt(50, 200),
+        getRandomInt(100, 150),
+        getRandomInt(150, 250)
+      ],
+      backgroundColor: ['#CCC', '#36A2EB', '#FFCE56'],
+      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+    }
+  ]
+});
 
 class Poll extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chartData: this.getInitialState() };
+  }
 
-    constructor() {
-        super(props);
-        this.state = {
-            data = {
-                labels: ['Red', 'Green', 'Yellow'],
-                datasets: [
-                    {
-                        data: [300, 50, 100],
-                        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-                        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-                    }
-                ]
-            }
-        }
-    }
+  getInitialState() {
+    return getState();
+  }
+
+  componentWillMount() {
+    setInterval(() => {
+      this.setState({ chartData: getState() });
+    }, 5000);
+  }
+
   render() {
-      return (
-          <div className="chart">
-              <Doughnut data={this.state.chartData}
-                  options={{
-                  }}
-              />
+    return (
+      <div className='chart'>
+        <Doughnut data={this.state.chartData} options={{}} />
       </div>
-    )
+    );
   }
 }
 
