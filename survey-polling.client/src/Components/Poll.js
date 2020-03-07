dbee1067bf55916e564977324ea6aa0c1bc8761c
xanderdrinnan <xanderdrinnan1998@gmail.com>
2020-03-06 23:04:03 -0500

    Still broken

--- survey-polling.client/src/Components/Poll.js
+++ survey-polling.client/src/Components/Poll.js
@@ -1,24 +1,39 @@
 ﻿import React, { Component } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-  labels: ['Red', 'Green', 'Yellow'],
-  datasets: [
-    {
-      data: [300, 50, 100],
-      backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-      hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-    }
-  ]
-};
 
-export default class Poll extends Component {
+class Poll extends Component {
+
+    constructor() {
+        super(props);
+        this.state = {
+            data = {
+                labels: ['Red', 'Green', 'Yellow'],
+                datasets: [
+                    {
+                        data: [300, 50, 100],
+                        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                        hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+                    }
+                ]
+            }
+        }
+    }
   render() {
-    return (
-      <div>
-        <h2>Doughnut Example</h2>
-        <Doughnut data={data} />
+      return (
+          <div className="chart">
+              <Doughnut data={this.state.chartData}
+                  options={{
+                  }}
+              />
       </div>
-    );
+    )
   }
 }
+
+export default Poll;
+
+//morumUpdateChart(barNumber) {
+//    myBarChart.data.datasets[0].data[barNumber] = myBarChart.data.datasets[0].data[barNumber] + 1;
+//    myBarChart.update();
+//}
