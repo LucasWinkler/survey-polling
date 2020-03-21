294487ab1abdef7c4824ebf43f65b815e1de262c
Fernando <fernandoguardado981@gmail.com>
2020-03-21 18:36:50 -0400

    Most done designing the page, you currently can't make a poll yet

--- survey-polling.client/src/components/MPNavbar/MPNavbar.js
+++ survey-polling.client/src/components/MPNavbar/MPNavbar.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Form, Button, Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './MPNavbar.scss';
+import icon from '../../assets/images/morum_logo.png';
 
 export default function MPNavbar() {
     return (
       <div id="container">
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand>Morum OSS | Manage Poll</Navbar.Brand>
-        </Navbar>
+      <Navbar bg="light" expand="lg">
+        <img id="imgIcon" src={icon}/> 
+        <Navbar.Brand id="brand">Morum OSS | Manage Poll</Navbar.Brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+          <Form inline>
+            <Button id="btnStart" color="primary" size="lg">Start</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
       </div>
     );
   }
--- survey-polling.client/src/pages/ManagePoll/ManagePoll.js
+++ survey-polling.client/src/pages/ManagePoll/ManagePoll.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Button, Form } from 'react-bootstrap';
+import { Button, Form, FormGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./MangePoll.scss";
 import MPNavbar from './../../components/MPNavbar/MPNavbar';
@@ -16,16 +16,33 @@ export default function ManagePoll() {
       <div id="container">
         <MPNavbar/>
         <div class="row">
-          <div class="questionColumn">
+          <div class="questionColumn" style={{}}>
             <h2>Questions</h2>
+            <br></br>
             <Form onSubmit={addQuestion}>
-              <Button variant="primary" type="submit">Add</Button>
+              <br></br>
+              <div class="input-group mb-3">
+                <Form.Control id="txtQuestion" type="text" placeholder="Enter a question" />
+              <div class="input-group-append">
+                <Button id="btnDelete" variant="danger" type="button">Delete</Button>
+              </div>
+              <br></br>
+            </div>
+            <Button id="btnAddQuestion" variant="primary" size="lg" type="submit">Add</Button>
             </Form>
         </div>
         <div class="optionsColumn">
             <h2>Your Question</h2>
-            <Form onSubmit={addOptions}>
-              <Button variant="primary" type="submit">Add</Button>
+            <br></br>
+              <Form onSubmit={addOptions}>
+              <Form.Control id="txtQuestionOptions" type="text" placeholder="Enter your question" />
+              <br></br>
+              <Button id="btnAddOption" variant="primary" size="lg" type="submit">Add</Button>
+              <br></br>
+              <h2>Options</h2>
+              <Form.Control id="txtOptions" type="text" placeholder="Option 1" /> 
+              <br></br>
+              <Button id="btnOptions" variant="primary" size="lg" type="submit">Add</Button>
             </Form>
         </div>
         </div>
