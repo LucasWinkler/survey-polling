4869733c289f191951abb6db940e0cac52b404eb
Fernando <fernandoguardado981@gmail.com>
2020-04-15 16:43:54 -0400

    Added logo to the join page

--- survey-polling.client/src/pages/Join/Join.js
+++ survey-polling.client/src/pages/Join/Join.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import './Join.scss';
-
+import logo from '../../assets/images/morum_logo.png';
 export default function Join(props) {
   const [pin, setPin] = useState('');
 
@@ -21,7 +21,9 @@ export default function Join(props) {
 
   return (
     <div className='join'>
+
       <div className='container_join__wrapper'>
+      <img src={logo} alt='Morum OSS Logo' className='home__logo' />
         <h1 className='join__title'>Please enter the pin:</h1>
         <form onSubmit={joinLobby}>
           <input
