e5b49876317f55287e1a2e90eec1bb2c737c3ecc
Fernando <fernandoguardado981@gmail.com>
2020-04-18 11:37:13 -0400

    Added image on top of poll over screen

--- survey-polling.client/src/pages/PollOver/PollOver.js
+++ survey-polling.client/src/pages/PollOver/PollOver.js
@@ -1,6 +1,6 @@
 import React, { useEffect, } from 'react';
-import {Redirect, Link} from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/morum_logo.png';
 import './PollOver.scss';
 import Join from './../Join/Join';
 
@@ -13,7 +13,8 @@ export default function PollOver(props) {
     <div className='pollover'>
     <div className="container pollover">
     <section className="pollover__title">
-      <h1 className="pollover__over">The poll is over</h1>
+    <img src={logo} alt='Morum OSS Logo' className='pollover__join__logo' />
+    <h1 className="pollover__over">The poll is over</h1>
       <br></br>
       <p className="pollover__thanks">Thanks for participating in the poll!</p>
       <br></br>
@@ -23,7 +24,6 @@ export default function PollOver(props) {
       <Link className="pollover__link" to="/join">Back</Link>
       </button>
     </section>
-
     </div>
     </div>
   );
